086290330cb1b030f55d83d7d5f4b80dbcd5302d
ElvinAbdullayevv <elvin.abdullayev.new@gmail.com>
2024-01-01 15:21:25 +0400

    Initial commit

---
+++ Gametime.py
@@ -0,0 +1,12 @@
+# import random as r
+
+# user1 = int(input("Guess "))
+# user2 = int(input("Guess "))
+
+# sysNumber = r.randrange(1,100)
+# print(sysNumber)
+
+# if (abs(user2 - sysNumber)) < (abs(user1 - sysNumber)):
+#     print("user2 won")
+# elif (abs(user2 - sysNumber)) > (abs(user1 - sysNumber)):
+#     print('user1 won')
---
+++ numberGame.py
@@ -0,0 +1,26 @@
+# a = int(input())
+# x = 1
+# aa=[]
+# while x <= a:
+#     if pow(x,2) <= a:
+#         aa.append(pow(x,2))
+#         x+= 1
+#     else:
+#         break
+#
+# print(*aa)
+
+# import random as r
+
+# user1 = int(input("Guess "))
+# user2 = int(input("Guess "))
+
+# sysNumber = r.randrange(1,100)
+# print(sysNumber)
+
+# if (abs(user2 - sysNumber)) < (abs(user1 - sysNumber)):
+#     print("user2 won")
+# elif (abs(user2 - sysNumber)) > (abs(user1 - sysNumber)):
+#     print('user1 won')
+
+
